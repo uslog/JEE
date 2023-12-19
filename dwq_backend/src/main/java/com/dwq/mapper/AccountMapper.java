98fd85283c8c2f08866bf65574c6e885c29ddb6f
PassAllProject <129824273+PassAllProject@users.noreply.github.com>
2023-12-19 11:23:05 +0800

    Delete dwq_backend directory

--- dwq_backend/src/main/java/com/dwq/mapper/AccountMapper.java
+++
@@ -1,8 +0,0 @@
-package com.dwq.mapper;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.dwq.entity.dto.Account;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface AccountMapper extends BaseMapper<Account> {
-}
